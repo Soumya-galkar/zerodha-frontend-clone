9af1cbc9793024f07226b50dc9506e3a29adb093
SoumyaGalkar <soumyagalkar@gmail.com>
2026-06-04 01:20:51 +0530

    Create AboutPage component with Navbar, Hero, Team, and Footer

---
+++ src/landing_page/about/AboutPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Team from './Team';
+import Hero from './Hero';
+import Navbar from '../../Navbar';
+import Footer from '../../Footer'
+function AboutPage() {
+    return (  
+<>
+<Navbar/>
+<Hero/>
+<Team/>
+<Footer/>
+</>
+    );
+
+}
+export default AboutPage;
